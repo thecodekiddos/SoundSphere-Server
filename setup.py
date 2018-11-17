0a02b4e52b8fd3cbade4dc2df45420e8dd98dfd0
connorjsheedy <connorjsheedy@gmail.com>
2018-11-17 09:24:08 -0600

    updated setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='flaskr',
+    name='soundsphere',
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
